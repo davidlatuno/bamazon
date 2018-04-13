7f2537052090ac5582b5342d850808d54b1f5e2c
davidlatuno <dslatuno@gmail.com>
2018-04-12 20:09:11 -0700

    Initial inquirer prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
-var {table} = require("table");
-var inquire = require("inquirer");
+var { table } = require("table");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-connection.query("SELECT * FROM products", function(err, res) {
+connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
     data = [["ID", "NAME", "DEPARMENT", "PRICE", "QUANTITY"]];
     for (var i = 0; i < res.length; i++) {
@@ -18,4 +18,53 @@ connection.query("SELECT * FROM products", function(err, res) {
     }
     var result = table(data);
     console.log(result);
-})
+    intro();
+});
+
+function intro() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What would you like to do?",
+                choices: ["BUY", "EXIT"],
+                name: "first"
+            }
+        ])
+        .then(function (input) {
+            if (input.first === "BUY") {
+                buyID();
+            } else {
+                connection.end();
+            }
+        })
+};
+
+function buyID() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is the ID of the item you would like to buy?",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "How many units would you like to buy?",
+                name: "number"
+            },
+            {
+                message: "Are you sure?",
+                type: "confirm",
+                name: "confirm",
+                default: true
+            }
+        ])
+        .then(function (input) {
+            if (!input.confirm) {
+                buyID();
+            } else {
+                
+            }
+        })
+}
